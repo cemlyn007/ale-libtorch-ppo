daea897d06826a29a107e118773fa51f7d1ffc5b
Cemlyn Neil Waters <cemlynwaters@gmail.com>
2025-06-05 21:38:59 +0100

    ci: compile_commands.json

--- src/ale_example.cc
+++ src/ale_example.cc
@@ -1,19 +1,16 @@
-#include "ale/ale_interface.hpp"
+#include <ale/ale_interface.hpp>
+#include <ale/version.hpp>
 #include <iostream>
 
-int main() {
+int main(int argc, char **argv) {
+  auto path = argv[1];
   ale::ALEInterface ale;
-
-  // Set ROM file
   std::cout << "Initializing Arcade Learning Environment..." << std::endl;
-  ale.loadROM(
-      "/home/cemlyn/Development/ale-bazel/roms/breakout.bin"); // You'll need to
-                                                               // provide an
-                                                               // Atari ROM file
-
+  ale.loadROM(path);
   std::cout << "ALE initialized!" << std::endl;
   std::cout << "Number of available actions: "
             << ale.getMinimalActionSet().size() << std::endl;
-
+  ale.reset_game();
+  std::cout << "Game reset!" << std::endl;
   return 0;
 }

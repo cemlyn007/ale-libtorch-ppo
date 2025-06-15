043ac4111cb3a160395ca2c56f5207bb4de753cc
Cemlyn Neil Waters <cemlynwaters@gmail.com>
2025-06-15 21:24:28 +0100

    ci: flamegraph profile script

---
+++ scripts/flamegraph.sh
@@ -0,0 +1,31 @@
+set -e
+
+workspace=$(bazel info workspace)
+
+tmp_directory=/tmp/ale-bazel-flamegraph
+perf_file_path=$tmp_directory/perf.data
+svg_file_path=$tmp_directory/perf.svg
+
+# -fno-omit-frame-pointer is advised from http://www.brendangregg.com/FlameGraphs/cpuflamegraphs.html#Instructions.
+bazel build \
+  --compilation_mode=dbg \
+  --copt=-fno-omit-frame-pointer \
+  --copt=-g \
+  --linkopt=-fno-omit-frame-pointer \
+  --strip=never \
+  //src:ale_libtorch_example
+
+mkdir -p $tmp_directory
+echo "Require sudo to run perf:"
+trap 'echo -e "\nProgram stopped by user. Continuing with flamegraph generation..."' SIGINT
+sudo perf record -a -b -g --output=$perf_file_path $workspace/bazel-bin/src/ale_libtorch_example $workspace/roms/breakout.bin $workspace/logs/perf || true
+trap - SIGINT
+sudo chown $USER $perf_file_path
+
+perf script --input=$perf_file_path | $workspace/third_party/FlameGraph/stackcollapse-perf.pl | $workspace/third_party/FlameGraph/flamegraph.pl > $svg_file_path
+
+if [ -x "$(command -v google-chrome)" ]; then
+  google-chrome $svg_file_path
+fi
+echo "Perf data recorded at $perf_file_path"
+echo "Flamegraph generated at $svg_file_path"

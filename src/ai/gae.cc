001572fc8f9bf08672bf200dc794a36e35eb077f
Cemlyn Neil Waters <cemlynwaters@gmail.com>
2025-07-05 16:54:17 +0100

    refactor: gae

--- src/ai/gae.cc
+++ src/ai/gae.cc
@@ -19,54 +19,56 @@ void gae(torch::Tensor &advantages, const torch::Tensor &rewards,
     throw std::invalid_argument(
         "Input tensors must have compatible dimensions.");
   }
-  auto expected_device = rewards.device();
+  auto expected_device = advantages.device();
+  if (rewards.device() != expected_device) {
+    throw std::invalid_argument("Rewards tensor must be on the same device as "
+                                "advantages tensor.");
+  }
   if (values.device() != expected_device) {
     throw std::invalid_argument("Values tensor must be on the same device as "
-                                "rewards tensor.");
+                                "advantages tensor.");
   }
   if (next_values.device() != expected_device) {
     throw std::invalid_argument("Next values tensor must be on the same device "
-                                "as rewards tensor.");
+                                "as advantages tensor.");
   }
   if (terminals.device() != expected_device) {
     throw std::invalid_argument(
         "Terminals tensor must be on the same device as "
-        "rewards tensor.");
+        "advantages tensor.");
   }
   if (truncations.device() != expected_device) {
     throw std::invalid_argument("Truncations tensor must be on the same device "
-                                "as rewards tensor.");
+                                "as advantages tensor.");
   }
   if (episode_starts.device() != expected_device) {
     throw std::invalid_argument("Episode starts tensor must be on the same "
-                                "device as rewards tensor.");
+                                "device as advantages tensor.");
   }
   auto total_environments = rewards.size(0);
   auto num_steps = rewards.size(1);
   auto last_advantages = torch::zeros(total_environments, rewards.options());
   torch::Tensor nv = next_values;
   for (int64_t i = num_steps - 1; i >= 0; --i) {
-    advantages.index_put_(
-        {torch::indexing::Slice(), i},
-        torch::where(episode_starts.index({torch::indexing::Slice(), i}), 0.0,
-                     rewards.index({torch::indexing::Slice(), i}) + gamma * nv -
-                         values.index({torch::indexing::Slice(), i}) +
-                         gamma * lambda * last_advantages));
-    advantages.index_put_(
-        {torch::indexing::Slice(), i},
-        torch::where(terminals.index({torch::indexing::Slice(), i}),
-                     rewards.index({torch::indexing::Slice(), i}) -
-                         values.index({torch::indexing::Slice(), i}),
-                     advantages.index({torch::indexing::Slice(), i})));
+    auto advantage_reset = 0.0;
+    auto advantage_running = rewards.select(1, i) + gamma * nv -
+                             values.select(1, i) +
+                             gamma * lambda * last_advantages;
+    auto advantage_terminal = rewards.select(1, i) - values.select(1, i);
+    auto advantage_truncation =
+        rewards.select(1, i) + gamma * nv - values.select(1, i);
 
-    advantages.index_put_(
-        {torch::indexing::Slice(), i},
-        torch::where(truncations.index({torch::indexing::Slice(), i}),
-                     rewards.index({torch::indexing::Slice(), i}) + gamma * nv -
-                         values.index({torch::indexing::Slice(), i}),
-                     advantages.index({torch::indexing::Slice(), i})));
-    last_advantages = advantages.index({torch::indexing::Slice(), i});
-    nv = values.index({torch::indexing::Slice(), i});
+    auto advantage = torch::where(episode_starts.select(1, i), advantage_reset,
+                                  advantage_running);
+    advantage =
+        torch::where(terminals.select(1, i), advantage_terminal, advantage);
+    advantage =
+        torch::where(truncations.select(1, i), advantage_truncation, advantage);
+
+    advantages.select(1, i).copy_(advantage);
+
+    last_advantages = advantage;
+    nv = values.select(1, i);
   }
 }
 

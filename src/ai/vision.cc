bd82768ffc5b6b79207c6e2c74725f0336c27fdd
Cemlyn Neil Waters <cemlynwaters@gmail.com>
2025-07-27 11:40:09 +0100

    test: fix vision test

--- src/ai/vision.cc
+++ src/ai/vision.cc
@@ -4,13 +4,14 @@
 
 namespace ai::vision {
 
+const auto options = torch::nn::functional::InterpolateFuncOptions()
+                         .size(std::vector<int64_t>({84, 84}))
+                         .mode(torch::kArea);
+
 torch::Tensor resize_grayscale_image(const torch::Tensor &image) {
   assert(image.dim() == 2);
   assert(image.size(0) == 210);
   assert(image.size(1) == 160);
-  const auto options = torch::nn::functional::InterpolateFuncOptions()
-                           .size(std::vector<int64_t>({84, 84}))
-                           .mode(torch::kArea);
   const std::array<int64_t, 4> size = {1, 1, 210, 160};
   const auto &input = image.view(size);
   const auto out = torch::nn::functional::interpolate(input, options);
@@ -24,9 +25,6 @@ resize_frame_stacked_grayscale_images(const torch::Tensor &images) {
   const auto frame_stack = images.size(1);
   assert(images.size(2) == 210);
   assert(images.size(3) == 160);
-  const auto options = torch::nn::functional::InterpolateFuncOptions()
-                           .size(std::vector<int64_t>({84, 84}))
-                           .mode(torch::kArea);
   const auto input = images.flatten(0, 1).unsqueeze(1);
   const auto out = torch::nn::functional::interpolate(input, options);
   return out.squeeze(1).unflatten(0, {batch_size, frame_stack});
--- test/ai/vision-test.cc
+++ test/ai/vision-test.cc
@@ -3,14 +3,14 @@
 #include <torch/torch.h>
 
 TEST(VisionTest, ResizeGrayscaleImage) {
-  torch::Tensor input = torch::ones({210, 160}, torch::kByte);
+  torch::Tensor input = torch::ones({210, 160}, torch::kFloat32);
   torch::Tensor output = ai::vision::resize_grayscale_image(input);
   EXPECT_EQ(output.sizes(), std::vector<int64_t>({84, 84}));
   EXPECT_TRUE((output == 1).all().item<bool>());
 }
 
 TEST(VisionTest, ResizeGrayscaleImages) {
-  torch::Tensor input = torch::empty({1, 4, 210, 160}, torch::kByte);
+  torch::Tensor input = torch::empty({1, 4, 210, 160}, torch::kFloat32);
   for (uint8_t i = 0; i < 4; ++i) {
     EXPECT_EQ(input.index({0, i}).sizes(), std::vector<int64_t>({210, 160}));
     input.index_put_({0, i}, i);
@@ -22,7 +22,7 @@ TEST(VisionTest, ResizeGrayscaleImages) {
       ai::vision::resize_frame_stacked_grayscale_images(input);
   EXPECT_EQ(output.sizes(), std::vector<int64_t>({1, 4, 84, 84}));
   EXPECT_EQ(output.sizes(), std::vector<int64_t>({1, 4, 84, 84}));
-  EXPECT_EQ(output.dtype(), torch::kByte);
+  EXPECT_EQ(output.dtype(), torch::kFloat32);
   for (int i = 0; i < 4; ++i) {
     auto output_data = output.index({0, i});
     EXPECT_EQ(output_data.sizes(), std::vector<int64_t>({84, 84}));
